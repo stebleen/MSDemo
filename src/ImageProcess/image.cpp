969a0b382fa8ab56587bf3542bec61f3b89a4f1b
Yuxuan Jiang <126655217+stebleen@users.noreply.github.com>
2024-06-26 17:01:26 +0800

    update

--- src/ImageProcess/image.cpp
+++ src/ImageProcess/image.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 extern "C" __declspec(dllexport) bool CompressImage(const char* inputPath, const char* outputPath)
 {
@@ -16,24 +17,23 @@ extern "C" __declspec(dllexport) bool CompressImage(const char* inputPath, const
         return false;
     }
 
-    char buffer[1024];
-    while (inputFile.read(buffer, sizeof(buffer))) {
-        // 简单的压缩算法：每个字节减半
-        for (std::streamsize i = 0; i < inputFile.gcount(); ++i) {
-            buffer[i] /= 2;
-        }
-        outputFile.write(buffer, inputFile.gcount());
-    }
-
-    // 处理最后一块数据
-    if (inputFile.gcount() > 0) {
-        for (std::streamsize i = 0; i < inputFile.gcount(); ++i) {
-            buffer[i] /= 2;
-        }
-        outputFile.write(buffer, inputFile.gcount());
-    }
-
+    // 读取输入文件的像素数据
+    std::vector<unsigned char> pixels;
+    inputFile.seekg(0, std::ios::end);
+    size_t fileSize = inputFile.tellg();
+    pixels.resize(fileSize);
+    inputFile.seekg(0, std::ios::beg);
+    inputFile.read(reinterpret_cast<char*>(&pixels[0]), fileSize);
     inputFile.close();
+
+    // 压缩算法：简单地将每个像素值减小一点
+    const int compressionFactor = 10; // 压缩因子，可以根据需要调整
+    for (size_t i = 0; i < pixels.size(); ++i) {
+        pixels[i] -= compressionFactor;
+    }
+
+    // 写入压缩后的像素数据到输出文件
+    outputFile.write(reinterpret_cast<const char*>(&pixels[0]), fileSize);
     outputFile.close();
 
     return true;
--- src/MS.WebApi/Controllers/admin/CommonController.cs
+++ src/MS.WebApi/Controllers/admin/CommonController.cs
@@ -6,6 +6,7 @@ using MS.Services;
 using Renci.SshNet.Compression;
 using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Ubiety.Dns.Core;
 
@@ -24,6 +25,10 @@ namespace MS.WebApi.Controllers.admin
             _aliOssService = aliOssService;
         }
 
+        [DllImport("D:\\program\\dotnet\\code\\final\\MSDemo\\src\\ImageProcess\\ImageProcess.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool CompressImage(string inputPath, string outputPath);
+
+
         [HttpPost("upload")]
         public async Task<IActionResult> UploadFile(IFormFile file)
         {
@@ -34,43 +39,60 @@ namespace MS.WebApi.Controllers.admin
 
             var objectName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
 
-            /*
-            var tempFilePath = Path.Combine(Path.GetTempPath(), objectName);
-            var compressedFilePath = Path.Combine(Path.GetTempPath(), "compressed_" + objectName);
-            using (var stream = new FileStream(tempFilePath, FileMode.Create))
+            using (var stream = file.OpenReadStream())
             {
-                await file.CopyToAsync(stream);
+                var fileUrl = await _aliOssService.UploadFileAsync(stream, objectName);
+                return Ok(new { code = true, data = fileUrl, msg = "文件上传成功" });
             }
-            // 调用C++/CLI封装的压缩函数
-            bool success = Compressor.CompressImage(tempFilePath, compressedFilePath);
-            if (!success)
-            {
-                return BadRequest(new { code = false, msg = "图片压缩失败" });
-            }
-            */
 
             /*
+            // 保存上传的图片到项目目录
+            var uploadsDirectory = Path.Combine(Directory.GetCurrentDirectory(), "uploads");
+            var compressedDirectory = Path.Combine(Directory.GetCurrentDirectory(), "compressed");
+
+            if (!Directory.Exists(uploadsDirectory))
+            {
+                Directory.CreateDirectory(uploadsDirectory);
+            }
+
+            if (!Directory.Exists(compressedDirectory))
+            {
+                Directory.CreateDirectory(compressedDirectory);
+            }
+
+            var inputPath = Path.Combine(uploadsDirectory, objectName);
+            var outputPath = Path.Combine(compressedDirectory, objectName);
 
             try
             {
-                var tempFilePath = Path.Combine(Path.GetTempPath(), objectName);
-                var compressedFilePath = Path.Combine(Path.GetTempPath(), "compressed_" + objectName);
 
-                using (var stream = new FileStream(tempFilePath, FileMode.Create))
+                using (var stream = file.OpenReadStream())
                 {
-                    await file.CopyToAsync(stream);
-                }
+                    using (var fileStream = new FileStream(inputPath, FileMode.Create))
+                    {
+                        await stream.CopyToAsync(fileStream);
+                    }
 
-                // 调用C++/CLI封装的压缩函数
-                bool success = Compressor.CompressImage(tempFilePath, compressedFilePath);
-                if (!success)
-                {
-                    return BadRequest(new { code = false, msg = "图片压缩失败" });
-                }
+                    if (CompressImage(inputPath, outputPath))
+                    {
+                        // 上传压缩后的文件到OSS或其他存储服务
+                        var compressedStream = new FileStream(outputPath, FileMode.Open);
+                        var fileUrl = await _aliOssService.UploadFileAsync(compressedStream, "compressed_" + objectName);
 
-            }
-            catch (Exception ex)
-            {
+                        // 删除原始图片
+                        if (System.IO.File.Exists(inputPath))
+                        {
+                            System.IO.File.Delete(inputPath);
+                        }
+                        return Ok(new { code = true, data = fileUrl, msg = "文件上传成功" });
+                    }
+                    else
+                    {
+                        return BadRequest(new { code = false, msg = "图片压缩失败" });
+                    }
+                }
+            } catch (Exception ex) {
+                Console.WriteLine(ex.ToString());
                 using (var stream = file.OpenReadStream())
                 {
                     var fileUrl = await _aliOssService.UploadFileAsync(stream, objectName);
@@ -79,12 +101,9 @@ namespace MS.WebApi.Controllers.admin
             }
             */
 
+                    
 
-            using (var stream = file.OpenReadStream())
-            {
-                var fileUrl = await _aliOssService.UploadFileAsync(stream, objectName);
-                return Ok(new { code = true, data = fileUrl, msg = "文件上传成功" });
-            }
+                   
         }
 
         /*
